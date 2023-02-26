4cf6dd66b1746902df3243e83c19d75a509bc357
xkrsx <98549349+xkrsx@users.noreply.github.com>
2023-02-26 18:17:19 +0100

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,23 @@
 import {createPool} from "mysql2/promise";
+import { config } from "../config/config";
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'megak_ads',
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbDatabase,
     namedPlaceholders: true,
     decimalNumbers: true,
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-})
+});
+
+// import {createPool} from "mysql2/promise";
+
+// export const pool = createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'megak_ads',
+//     namedPlaceholders: true,
+//     decimalNumbers: true,
+//     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+// })
